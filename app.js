18071966bb0c9ae6fddcfdbbde86e312abbfdf41
Brian Soe <bsoe@ucsd.edu>
2015-04-06 16:49:28 -0700

    init for boilerplate assignment 1

--- app.js
+++ app.js
@@ -2,32 +2,26 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-var handlebars = require('express3-handlebars');
+var handlebars = require('express-handlebars');
+var bodyParser = require('body-parser');
 var app = express();
+
 //route for hashtag
-var hashtag = require('./routes/hashtag');
 var index = require('./routes/index');
-//database setup
-var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGOHQ_URL || 'mongodb://localhost/instagramexample');
 
 //Configures the Template engine
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.bodyParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //routes
 app.get('/', index.view);
-app.get('/hashtag', function (req, res) {
-	res.render('hashtag');
-})
-app.post('/hashtag', hashtag.getHashtag);
-app.post('/save', hashtag.saveFavorites);
-app.post('/delete', index.deleteImage);
+
 //set environment ports and start application
 app.set('port', process.env.PORT || 3000);
-http.createServer(app).listen(app.get('port'), function(){
-	console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,4 @@
-var models = require('../models');
-
 exports.view = function(req, res) {
-	//search the database object for a model.
-	models.Img.find({}, function(err, dbData){
-		data = {'images':dbData}
-		res.render('index', data);
-	});
-}
-
-exports.deleteImage = function(req, res) {
-	models.Img.find({ _id: req.body.id }).remove().exec();
-	res.redirect('/');
+	//TODO
+	res.render('index');
 }
